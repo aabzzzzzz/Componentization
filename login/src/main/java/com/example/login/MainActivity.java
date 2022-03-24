913cff21c0d2a11355665b4b181a4de391faf562
aabzzzzzz <2283304981@qq.com>
2022-03-25 01:50:49 +0800

    自定义  BuildConfig中的参数, BuildConfig.isRelease; 配置测试环境的url和正式环境的url.

--- login/src/main/java/com/example/login/MainActivity.java
+++ login/src/main/java/com/example/login/MainActivity.java
@@ -10,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        serverURL = BuildConfig.isRelease;
+//        Toast.makeText(this, "serverURL:" + serverURL, Toast.LENGTH_SHORT).show();
+//
+        if (BuildConfig.isRelease) {
+//            Log.d(TAG, "onCreate: 当前是：集成化 线上环境，以app壳为主导运行的方式");
+//            Toast.makeText(this, "当前是：集成化 线上环境，以app壳为主导运行的方式", Toast.LENGTH_SHORT).show();
+        } else {
+//            Log.d(TAG, "onCreate: 当前是：组件化 测试环境，所有的子模块都可以独立运行");
+//            Toast.makeText(this, "当前是：组件化 测试环境，所有的子模块都可以独立运行", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
